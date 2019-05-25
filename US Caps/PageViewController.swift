550ca7b4c078acc18735635363a16b51898ce209
Kevin Jackson <kj31428@gmail.com>
2019-05-25 08:53:20 -0700

    Set ups page view drawing.

---
+++ US Caps/PageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PageViewController.swift
+//  US Caps
+//
+//  Created by Kevin Jackson on 5/25/19.
+//  Copyright © 2019 Kevin Jackson. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+    
+    var worldStateController: WorldStateController!
+    
+    func update() {
+        print("pageViewUpdated()")
+    }
+}
---
+++ US Caps/PairViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PairViewController.swift
+//  US Caps
+//
+//  Created by Kevin Jackson on 5/25/19.
+//  Copyright © 2019 Kevin Jackson. All rights reserved.
+//
+
+import UIKit
+
+class PairViewController: UIViewController {
+    
+}
